ec232996faafe489073dc8c241d8573baddf5a26
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-01-10 15:49:12 -0600

    finished the exercise from zoo

---
+++ zoo.py
@@ -0,0 +1,23 @@
+# create a tuple named 'zoo' that contains your favorite animals:
+zoo = ("lion", "hippo", "flamingo")
+print("Here's the zoo tuple:", zoo)
+
+# find one of your animals using the .index(value) method on the tuple:
+print("This is the animal found with index 0:", zoo[0])
+
+# Determine if an animal is in your tuple by using a keyword:
+(lion, hippo, flamingo) = zoo
+print("Show me a 'hipppo':", hippo)
+
+# Convert your tuple into a list:
+my_list = list(zoo)
+print("This is my zoo list:", my_list)
+
+# Use extend() to add three more animals to your zoo
+more_animals = ['snake', 'monkey', 'giraffe']
+my_list.extend(more_animals)
+print("Here's a larger list of zoo animals:", my_list)
+
+# Convert the list back to a tuple:
+new_zoo = tuple(my_list)
+print("Here's the larger zoo list as a tuple:", new_zoo)
